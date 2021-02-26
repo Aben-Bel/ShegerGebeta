51fb0f722ebdb9a554921c7bd75f1426f09d9228
aben-bel <abenezer.sbelay@gmail.com>
2021-02-26 08:52:59 +0300

    implement CardView with in a RecyclerView

--- app/src/main/java/com/abenbel/sheger_gebeta/MainActivity.kt
+++ app/src/main/java/com/abenbel/sheger_gebeta/MainActivity.kt
@@ -2,6 +2,7 @@ package com.abenbel.sheger_gebeta
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,87 +10,97 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var layoutManager : RecyclerView.LayoutManager?=null
+    private var adapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        val data: Array<String> = arrayOf("1","2","3");
-        recyclerView.adapter = CustomeAdapter(data);
+        layoutManager = LinearLayoutManager(this);
+        val recyclerView:RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = RecyclerViewAdapter()
+        recyclerView.adapter = adapter;
+//        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+//        val data: Array<String> = arrayOf("1","2","3");
+//        recyclerView.adapter = CustomeAdapter(data);
 
     }
 }
 
-class CustomeAdapter(val dataSet: Array<String>) : RecyclerView.Adapter<CustomeAdapter.ViewHolder>() {
-        /**
-         * Provide a reference to the type of views that you are using
-         * (custom ViewHolder).
-         */
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView
-
-            init {
-                // Define click listener for the ViewHolder's View.
-                textView = view.findViewById(R.id.textView)
-            }
-        }
-
-        // Create new views (invoked by the layout manager)
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-            // Create a new view, which defines the UI of the list item
-            val view = LayoutInflater.from(viewGroup.context)
-                .inflate(R.layout.text_row_item, viewGroup, false)
-
-            return ViewHolder(view)
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-            viewHolder.textView.text = dataSet[position]
-
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        override fun getItemCount() = dataSet.size
-}
+//class CustomeAdapter(val dataSet: Array<String>) : RecyclerView.Adapter<CustomeAdapter.ViewHolder>() {
+//        /**
+//         * Provide a reference to the type of views that you are using
+//         * (custom ViewHolder).
+//         */
+//        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//            val textView: TextView
 //
-//class CustomAdapter( dataSet: Array<String>) :
-//    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-//
-//    /**
-//     * Provide a reference to the type of views that you are using
-//     * (custom ViewHolder).
-//     */
-//    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-//        val textView: TextView
-//
-//        init {
-//            // Define click listener for the ViewHolder's View.
-//            textView = view.findViewById(R.id.textView)
+//            init {
+//                // Define click listener for the ViewHolder's View.
+//                textView = view.findViewById(R.id.textView)
+//            }
 //        }
-//    }
 //
-//    // Create new views (invoked by the layout manager)
-//    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-//        // Create a new view, which defines the UI of the list item
-//        val view = LayoutInflater.from(viewGroup.context)
-//            .inflate(R.layout.text_row_item, viewGroup, false)
+//        // Create new views (invoked by the layout manager)
+//        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+//            // Create a new view, which defines the UI of the list item
+//            val view = LayoutInflater.from(viewGroup.context)
+//                .inflate(R.layout.text_row_item, viewGroup, false)
 //
-//        return ViewHolder(view)
-//    }
+//            return ViewHolder(view)
+//        }
 //
-//    // Replace the contents of a view (invoked by the layout manager)
-//    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+//        // Replace the contents of a view (invoked by the layout manager)
+//        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 //
-//        // Get element from your dataset at this position and replace the
-//        // contents of the view with that element
-//        viewHolder.textView.text = dataSet[position]
-//    }
+//            // Get element from your dataset at this position and replace the
+//            // contents of the view with that element
+//            viewHolder.textView.text = dataSet[position]
 //
-//    // Return the size of your dataset (invoked by the layout manager)
-//    override fun getItemCount() = dataSet.size
+//        }
 //
+//        // Return the size of your dataset (invoked by the layout manager)
+//        override fun getItemCount() = dataSet.size
 //}
+////
+////class CustomAdapter( dataSet: Array<String>) :
+////    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+////
+////    /**
+////     * Provide a reference to the type of views that you are using
+////     * (custom ViewHolder).
+////     */
+////    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+////        val textView: TextView
+////
+////        init {
+////            // Define click listener for the ViewHolder's View.
+////            textView = view.findViewById(R.id.textView)
+////        }
+////    }
+////
+////    // Create new views (invoked by the layout manager)
+////    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+////        // Create a new view, which defines the UI of the list item
+////        val view = LayoutInflater.from(viewGroup.context)
+////            .inflate(R.layout.text_row_item, viewGroup, false)
+////
+////        return ViewHolder(view)
+////    }
+////
+////    // Replace the contents of a view (invoked by the layout manager)
+////    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+////
+////        // Get element from your dataset at this position and replace the
+////        // contents of the view with that element
+////        viewHolder.textView.text = dataSet[position]
+////    }
+////
+////    // Return the size of your dataset (invoked by the layout manager)
+////    override fun getItemCount() = dataSet.size
+////
+////}
---
+++ app/src/main/java/com/abenbel/sheger_gebeta/RecyclerViewAdapter.kt
@@ -0,0 +1,50 @@
+package com.abenbel.sheger_gebeta
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    private val itemTitles = arrayOf("Restuarant 1", "Restaurant 2")
+    private val itemDetails = arrayOf("textDesc1","textDesc2");
+    private val itemImages = intArrayOf(R.drawable.ic_launcher_background)
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val v: View = LayoutInflater.from(p0.context)
+            .inflate(R.layout.recyclerview_model, p0, false)
+        return ViewHolder(v);
+    }
+
+
+    override fun onBindViewHolder(p0: RecyclerViewAdapter.ViewHolder, p1: Int) {
+        p0.textTitle.text = itemTitles[p1]
+        p0.textDesc.text = itemDetails[p1]
+        p0.image.setImageResource(itemImages[0])
+    }
+
+    override fun getItemCount(): Int {
+        return itemTitles.size;
+    }
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image : ImageView
+        var textTitle : TextView
+        var textDesc : TextView
+        var favoriteBtn : Button
+
+        init {
+            image = itemView.findViewById(R.id.image_view);
+            textTitle = itemView.findViewById(R.id.text_title);
+            textDesc = itemView.findViewById(R.id.text_desc);
+            favoriteBtn = itemView.findViewById(R.id.favorite_btn)
+        }
+    }
+
+
+}
